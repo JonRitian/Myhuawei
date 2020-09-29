26aaf4697429e98a45544f2be7ec8112bf9405e0
Travel <Jon_Email@126.com>
2020-09-29 14:29:00 +0800

    编写业务类

---
+++ Myweb01/src/main/java/com/zx/service/UserService.java
@@ -0,0 +1,14 @@
+package com.zx.service;
+
+import com.zx.domain.User;
+
+public class UserService {
+
+    public User findUserByName(String s) {
+        return null;
+    }
+
+    public int login(User user) {
+        return 1;
+    }
+}
---
+++ Myweb01/src/test/java/com/zx/service/TestUserService.java
@@ -0,0 +1,48 @@
+package com.zx.service;
+
+import com.zx.domain.User;
+import org.junit.Test;
+
+public class TestUserService {
+     @Test
+    public  void test01(){
+         //将用户信息发到后台
+        UserService userService=new UserService();
+        //查找用户信息
+        User user=userService.findUserByName("jackma");
+        //根据查找到的数据，进行判断  正确 错误 不存在
+         if(user == null){
+             System.out.println("不存在");
+         }
+         else {
+             System.out.println("存在");
+         }
+    }
+
+    @Test
+    public  void test02(){
+        //将用户信息发到后台
+        UserService userService=new UserService();
+        //查找用户信息
+        User user=new User();
+        user.setUsername("jackma");
+        user.setPassword("123456");
+      int code=userService.login(user);
+        //根据查找到的数据，进行判断  正确 错误 不存在
+        if(code == -1){
+            System.out.println("未注册");
+        }
+        else if(code==1){
+            System.out.println("注册，已激活，密码正确");
+        }
+        else if(code==-2){
+            System.out.println("注册，未激活");
+        }
+        else if(code==-3){
+            System.out.println("注册，已激活，密码不正确");
+        }
+    }
+
+
+}
+
